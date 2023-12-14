8219c7b5796ad95708cbb09c0a75ccda8d7152ee
UserPierreG <pierrelouisguichard@gmail.com>
2023-12-14 18:33:01 +0000

    v1.3 Minor changes

--- src/MentalHealthPage.jsx
+++ src/MentalHealthPage.jsx
@@ -17,22 +17,36 @@ function MentalHealthPage() {
         When people experience a mental health issue, it usually means that they have difficulty in some or all of these areas. Having feelings of stress, anxiety, and low mood at times are completely normal; unfortunately, it is a part of everyday life! However, when these thoughts or feelings become frequent or so severe that they change our ability to cope or manage our lives, they can develop into a mental health problem. Because of this, they can affect our mental and emotional capabilities, our relationships, our views of ourselves, and our overall quality of life.
       </p>
       <p className="mental-health-btn"> 
-        <a class="btn btn-light" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Toggle first element</a>
-        <button class="btn btn-light" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Toggle second element</button>
-        <button class="btn btn-light" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Toggle both elements</button>
+        <a class="btn btn-light" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Breathing exercises</a>
+        <button class="btn btn-light" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Take some ‘me time’</button>
+        <button class="btn btn-light" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">General</button>
       </p>
       <div class="row">
         <div class="col">
           <div class="collapse multi-collapse" id="multiCollapseExample1">
             <div class="card card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            When we get stressed, angry or anxious, we tend to
+take shallow breaths that are quick. This means that
+our body is in ‘fight or flight’ mode. We can reverse
+this effect by changing the way that we breathe. Start
+to take slower, more controlled breaths, and count to
+three as you breathe in, and four as you breathe out.
+Try to breathe out a little more than you breathe in,
+and when you breathe in, imagine that your lungs are
+a sack that are filling up with fresh air. As you breathe
+out, try to breathe out the negative feelings and
+thoughts you are experiencing.
             </div>
           </div>
         </div>
         <div class="col">
           <div class="collapse multi-collapse" id="multiCollapseExample2">
             <div class="card card-body">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+            • Watch your favourite TV show <br/>
+• Listen to music<br/>
+• Take some annual leave to go travelling<br/>
+• Exercising<br/>
+• Taking a warm bath<br/>
             </div>
           </div>
         </div>
